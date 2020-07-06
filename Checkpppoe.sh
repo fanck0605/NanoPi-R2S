c7cad3fc7ca5f2d0e999073e465b67c4b75e2036
jsenetkx <43450604+jsenetkx@users.noreply.github.com>
2020-07-06 15:51:22 +0800

    Update Checkpppoe.sh

--- Checkpppoe.sh
+++ Checkpppoe.sh
@@ -10,19 +10,25 @@
 	while true
 	do
 		gw=$(getgw $1)
-		echo "Ping----${gw}-------"
+		if [ ${#gw} -lt 4 ];then
+			#echo "Get gateway error..."
+			ifup ${1}
+			sleep 3
+			continue
+		fi
+		#echo "Ping----${gw}-------"
 		ping -c 1 -W 1 $gw > /dev/null
 		if [ $? -eq 0 ];then
 			echo 'ok'
 			sleep 2
 		else
 			erri=$(( $erri + 1 ))
-			echo "Ping ateway error ${erri} times..."
+			#echo "Ping ateway error ${erri} times..."
 			sleep 1
 		fi
 		
 		if [ $erri -ge 3 ];then
-			echo "Gateway missing......re-dial.."
+			#echo "Gateway missing......re-dial.."
 			erri=0
 			ifdown ${1} && ifup ${1}
 			sleep 15
